c2833527bcb02822bf42d7e5444162d7ca1cf110
Tyler Johnson <tyler@vintyge.com>
2014-10-31 23:45:38 -0600

    added a more package manager like interface

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 
-var infinite = require("../"),
-	minimist = require("minimist");
+var minimist = require("minimist"),
+	spawn = require("child_process").spawn;
 
 var argv = minimist(process.argv.slice(2), {
-	strings: [ "modules" ],
-	boolean: [ "version", "production", "silent" ],
+	boolean: [ "version", "production" ],
 	alias: { v: "version", p: "production" },
-	default: { silent: false }
+	'--': true
 });
 
 if (argv.version) {
@@ -16,13 +15,53 @@ if (argv.version) {
 	process.exit(0);
 }
 
-infinite(argv._[0], {
-	production: argv.production,
-	node_modules: argv.modules,
-	loglevel: argv.silent ? "silent" : null
-}).then(function(res) {
-	if (!argv.silent) {
-		console.log("Installed dependencies in " + res[0].length + " packages.");
-		console.log("Symlinked " + res[1].length + " packages.");
-	}
-});
+switch(argv._[0]) {
+	case "add":
+		require("../lib/pm.js").add(argv._.slice(1)).then(function(names) {
+			if (!names.length) return console.log("No packages added.");
+			console.log("Added " + names.length + " package" + (names.length === 1 ? "" : "s" ) + ":");
+			names.forEach(function(n) { console.log(n); });
+		});
+		break;
+
+	case "remove":
+		require("../lib/pm.js").remove(argv._.slice(1)).then(function(names) {
+			if (!names.length) return console.log("No packages removed.");
+			console.log("Removed " + names.length + " package" + (names.length === 1 ? "" : "s" ) + ":");
+			names.forEach(function(n) { console.log(n); });
+		});
+		break;
+
+	case "list":
+		require("../lib/pm.js").list().then(function(names) {
+			if (!names.length) return console.log("No packages.");
+			names.forEach(function(n) { console.log(n); });
+		});
+		break;
+
+	case "start":
+		require("../lib/pm.js").list().then(function(names) {
+			var expr = "process.argv = " + JSON.stringify(argv["--"]) + ";";
+			expr += "process.infinite = true;";
+
+			expr += names.map(function(n) {
+				return "require('" + n.replace(/'/g, "\\'") + "');"
+			}).join("");
+			
+			var runtime = spawn("node", [ "-e", expr ]);
+			runtime.stdout.pipe(process.stdout);
+			runtime.stderr.pipe(process.stderr);
+		});
+		break;
+
+	case "install":
+		require("../lib/install.js")(argv._[1] || "./packages", {
+			npm: {
+				production: argv.production
+			}
+		}).then(function(res) {
+			console.log("Installed dependencies in " + res[0].length + " packages.");
+			console.log("Symlinked " + res[1].length + " packages.");
+		});
+		break;
+}
