042df79572fc41087d2f5806f7171534719c06cc
Tyler Johnson <tyler@vintyge.com>
2015-03-14 17:10:38 -0600

    started using the actual cwd

--- bin/cmd.js
+++ bin/cmd.js
@@ -34,6 +34,8 @@ if (argv._[0] == null || argv.help) {
 	return process.exit(0);
 }
 
+var cwd = process.cwd();
+
 function pkgdir(pkg) {
 	if (_.isArray(pkg)) return _.map(pkg, pkgdir);
 	return _.pick(pkg, "name", "__dirname");
@@ -41,7 +43,7 @@ function pkgdir(pkg) {
 
 switch(argv._[0]) {
 	case "add":
-		ipm.add(".", argv._.slice(1), argv).then(function(res) {
+		ipm.add(cwd, argv._.slice(1), argv).then(function(res) {
 			var names = pkgdir(res);
 			if (!names.length) return console.log("No packages added.");
 			console.log("\n  Added " + names.length + " package" + (names.length === 1 ? "" : "s" ) + ":");
@@ -52,7 +54,7 @@ switch(argv._[0]) {
 
 	case "rm":
 	case "remove":
-		ipm.remove(".", argv._.slice(1), argv).then(function(names) {
+		ipm.remove(cwd, argv._.slice(1), argv).then(function(names) {
 			if (!names.length) return console.log("No packages removed.");
 			console.log("\n  Removed " + names.length + " package" + (names.length === 1 ? "" : "s" ) + ":");
 			names.forEach(function(n) { console.log("    - %s", n); });
@@ -62,7 +64,7 @@ switch(argv._[0]) {
 
 	case "ls":
 	case "list":
-		ipm.list(".", argv).then(function(res) {
+		ipm.list(cwd, argv).then(function(res) {
 			var names = pkgdir(res);
 			if (!_.size(names)) return console.log("No packages.");
 			console.log("\n  Packages (%s):", ipm.infFileName);
@@ -72,7 +74,7 @@ switch(argv._[0]) {
 		break;
 
 	case "install":
-		require("../lib/install.js")(".", argv._.slice(1), argv).then(function(res) {
+		require("../lib/install.js")(cwd, argv._.slice(1), argv).then(function(res) {
 			var names = pkgdir(res);
 			if (!names.length) return console.log("No packages installed.");
 			console.log("\n  Installed " + names.length + " package" + (names.length === 1 ? "" : "s" ) + ":");
@@ -82,7 +84,7 @@ switch(argv._[0]) {
 		break;
 
 	case "watch":
-		require("../lib/watch.js")(".", argv._.slice(1), argv).then(function(res) {
+		require("../lib/watch.js")(cwd, argv._.slice(1), argv).then(function(res) {
 			var names = pkgdir(res);
 			if (!names.length) return console.log("No packages installed.");
 			console.log("\n  Watching " + names.length + " package" + (names.length === 1 ? "" : "s" ) + ":");
